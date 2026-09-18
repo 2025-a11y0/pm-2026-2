a2018cd0d97738b3ae5f5382ac64c3ad029d6a1e
Gonzalo <gonzaloracas2017@gmail.com>
2026-09-17 21:58:26 -0500

    Trabajo del dia Viernes 11/9/2026 (S3 - D2) - Cambios en el login, uso de CSS, footer, navbar y creación de register

--- website/controllers.js
+++ website/controllers.js
@@ -8,6 +8,12 @@ export function home(req, res) {
   });
 }
 
+export function register(req, res) {
+  return res.render('website/register', {
+    title: 'Crear cuenta',
+  });
+}
+
 export function about(req, res) {
   return res.render('website/about', {
     title: 'Acerca de',
@@ -42,7 +48,25 @@ export function signIn(req, res) {
   });
 }
 
-export async function login(req, res) {
+export function login(req, res) {
+  let fondos = [
+    "/assets/img/login-bg.png",
+    "/assets/img/login-bg2.png",
+    "/assets/img/login-bg3.jpg"
+  ];
+  return res.render('website/login', {
+    title: 'Bienvenido',
+    background: fondos[Math.floor(Math.random() * 3)]
+  });
+}
+
+export function resetPassword(req, res) {
+  return res.render('website/reset-password', {
+    title: 'Recuperar Contraseña',
+  });
+}
+
+export async function login2(req, res) {
   const { user, password } = req.body;
   const validUser = process.env.DEFAULT_USER || 'admin';
   const validPassword = process.env.DEFAULT_PASSWORD || '123';
